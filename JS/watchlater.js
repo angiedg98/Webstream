b1b6248f663380827250e65033917c89ee9d72e5
angiedg98 <21100291@virtualwindow.co.za>
2021-10-12 08:12:30 +0200

    watch later footer
    - added some js hover effects to buttons and social media links
    - removed text decoration from nav links

--- JS/watchlater.js
+++ JS/watchlater.js
@@ -1,3 +1,48 @@
 $(function(){
     console.log("JS ready to mingle")
+
+    // email icon
+   $("#email").hover(function() {
+        // changes image to white to display active
+
+        $(this).attr("src", "../images/emailwhite.png")
+        }, function (){
+
+            // changes back to original colour
+            $(this).attr("src", "../images/email.png")
+    });
+
+    // instagram icon
+   $("#instagram").hover(function() {
+    // changes image to white to display active
+
+        $(this).attr("src", "../images/instagramwhite.png")
+        }, function (){
+
+            // changes back to original colour
+            $(this).attr("src", "../images/instagram.png")
+    }); 
+
+    // youtube icon
+   $("#youtube").hover(function() {
+    // changes image to white to display active
+
+        $(this).attr("src", "../images/youtube white.png")
+        }, function (){
+
+            // changes back to original colour
+            $(this).attr("src", "../images/youtube.png")
+    }); 
+
+
+
+
+
+
+
+
+
+
+
+
 });
