1ee4f106ab7855241c48640ba34c0acef1f2134a
angiedg98 <21100291@virtualwindow.co.za>
2021-10-28 07:15:17 +0200

    watch later page
    - worked more on functionality
    - linked nav links to other pages

--- JS/landing.js
+++ JS/landing.js
@@ -9,12 +9,21 @@ $(function(){
 
   });
 
-  $("#login").on("click", function(){
+  // change form to signup
+  $("#signup").on("click", function(){
 
       //$(".four").css("margin-left", "0%");\
-      $(".slide").toggleClass("slide-slide");
+    //   $(".slide").toggleClass("slide-slide");
+    $(".slide-slide").addClass();
 
   });
+
+    // change form back to login
+    $("#login").on("click", function(){
+
+      $(".slide-slide").removeClass();
+  
+    });
 
   // functions for tablet sign up
 
@@ -22,5 +31,27 @@ $(function(){
       $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
    });
 
+   //redirect after login
+
+   $(".button").on("click", function(){
+        window.location.href = 'PAGES/home.html';
+
+   });
+
+   // change login image to sign up image
+   $("#signup").click(function(){
+    $(".featureTwo").slideUp(500);
+    $(".test").css("display", "block");
+    // $(".featureTwo").attr("src", "images/gradient.png");
+    $(".featureOne").text("Join Our Family!");
+});
+
+   // change back to login image
+   $("#login").click(function(){
+    $(".featureTwo").slideDown(500);
+    $(".test").css("display", "nones");
+    // $(".featureTwo").attr("src", "images/gradient.png");
+    $(".featureOne").text("Welcome Back!");
+});
 
 });
