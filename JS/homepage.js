9303198fff204f126ac1c06222e8e17c7446587c
Frikkie3006 <21100292@virtualwindow.co.za>
2021-11-04 20:24:06 +0200

    Header Slider functional
    Just need to add movie titles and descriptions

--- JS/homepage.js
+++ JS/homepage.js
@@ -11,7 +11,42 @@ $(function(){
 
     document.getElementsByClassName("headerSlider")
         console.log("Header container spotted");
-        
-    
+
+
+const track = document.querySelector('.carousel_track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel_button--right');
+const prevButton = document.querySelector('.carousel_button--left');
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+console.log(slideWidth);
+
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+}
+
+slides.forEach(setSlidePosition);
+
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+
+    moveToSlide(track, currentSlide, nextSlide);
+});
+
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+
+    moveToSlide(track, currentSlide, prevSlide);
+});
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
 });
 
